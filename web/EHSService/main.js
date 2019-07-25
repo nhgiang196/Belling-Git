5731b371a2517d9430ef690e31ba4ea19981aa3a
nhgiang196 <nhgiang196>
2019-07-25 14:44:06 +0700

    update EHSService/main.js

--- web/EHSService/main.js
+++ web/EHSService/main.js
@@ -1,6 +1,16 @@
 /**
  * Created by wang.chen on 2016/9/13.
  */
-define( ['./CompanyService','./MethodProcessService','./VoucherService','./WasteItemService'],function(){
+define( 
+    [
+        './CompanyService'
+        ,'./MethodProcessService'
+        ,'./VoucherService'
+        ,'./WasteItemService'
+        ,'./CReportService'
+        ,'./CReportDirective'
+        ,'controllers/EHS/CReport/ICReportDirective'
+        ,'controllers/EHS/CReport/ACReportDirective'
+    ],function(){
 
 })
